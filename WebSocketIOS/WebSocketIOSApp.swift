e3e8a3bf6f6aac5e480dedd607347f54c4d19b20
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-03-23 13:12:44 -0300

    Initial Commit

---
+++ WebSocketIOS/WebSocketIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketIOSApp.swift
+//  WebSocketIOS
+//
+//  Created by Gabriel on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
